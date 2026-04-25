ce41b4d86d5e3e650569044deba82a832a4ca450
Eeve3 <51075870+Eevee122@users.noreply.github.com>
2026-04-26 02:46:42 +0300

    timer.h

--- timer.h
+++ timer.h
@@ -12,11 +12,11 @@ class Timer
         void stop();
         unsigned int get_time();
 
-        //overloaded function
-        std::string get_time_string(); //returns the current time to string
-        std::string get_time_string(unsigned int t); //returns specified time to string
+        //Перегруженная функция
+        std::string get_time_string(); //Возвращает текущее время в строку
+        std::string get_time_string(unsigned int t); //Возвращает указанное время в строку
 
-        //forbids copy constructor to avoid warning in c++11
+        //Запрещает копировать конструктор, чтобы избежать предупреждения в c++11
         Timer(const Timer& t) = delete;
         Timer& operator=(const Timer& t) = delete;
     private:
